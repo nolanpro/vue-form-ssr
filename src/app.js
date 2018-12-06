f7055d624d11231a11eaf3b37aaefb52dfa9ea5d
Nolan Ehrstrom <github@nolanpro.com>
2018-12-06 15:35:57 -0800

    Wire up context so we can pass data from laravel

--- src/app.js
+++ src/app.js
@@ -1,20 +1,29 @@
 import Vue from 'vue'
 import App from './App.vue'
 import renderVueComponentToString from 'vue-server-renderer/basic'
-
 import FormText from "@processmaker/vue-form-builder/src/components/renderer/form-text.vue";
-
 Vue.component(FormText);
+
+if (typeof context == 'undefined') {
+  throw new Error(
+    "Global context is not defined. " +
+    "Are you running this using the spatie/server-side-rendering package?"
+  )
+}
+
 const app = new Vue({
   el: '#app',
-  render: h => h(App)
+  render: h => h(App, {
+    props: {
+      config: context.config,
+      formData: context.data
+    }
+  })
 })
-
-// app.use()
 
 renderVueComponentToString(app, (err, html) => {
   if (err) {
-    console.log("ERROR!!!!!!", err)
+    throw err
   }
   console.log(html)
 })
